578315f0c03a44eeaec17501007c22855d146832
wesly <weslyambarita4@gmail.com>
2026-02-26 20:19:01 +0700

    update

--- app/src/main/java/org/delcom/pam_p4_ifs23013/module/AnimalModule.kt
+++ app/src/main/java/org/delcom/pam_p4_ifs23013/module/AnimalModule.kt
@@ -7,17 +7,20 @@ import dagger.hilt.components.SingletonComponent
 import org.delcom.pam_p4_ifs23013.network.animals.service.IAnimalAppContainer
 import org.delcom.pam_p4_ifs23013.network.animals.service.IAnimalRepository
 import org.delcom.pam_p4_ifs23013.network.animals.service.AnimalAppContainer
-import org.delcom.pam_p4_ifs23013.network.animals.service.AnimalRepository
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AnimalModule {
+
     @Provides
+    @Singleton
     fun provideAnimalContainer(): IAnimalAppContainer {
         return AnimalAppContainer()
     }
 
     @Provides
+    @Singleton
     fun provideAnimalRepository(container: IAnimalAppContainer): IAnimalRepository {
         return container.animalRepository
     }
